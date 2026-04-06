82c43ae6ab0ca0aa7f73e5851bf5adbbe93c42e1
Pedro <pedrocsilva2004@gmail.com>
2026-04-06 20:49:13 -0300

    craição da classe jogo

---
+++ Supertrunfo/Jogo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Supertrunfo
+{
+    public class Jogo
+    {
+        public Jogador Jogador1 { get; set; }
+        public Jogador Jogador2 { get; set; }
+
+        public Jogo(Jogador jogador1, Jogador jogador2)
+        {
+            Jogador1 = jogador1;
+            Jogador2 = jogador2;
+        }
+    }
+}
--- Supertrunfo/Program.cs
+++ Supertrunfo/Program.cs
@@ -3,7 +3,10 @@ using Supertrunfo;
 
 Carta carta1 = new Carta("Neymar", "Atacante", 88, 85, 83, 50);
 Jogador jogador1 = new Jogador("Pedro");
+Jogador jogador2 = new Jogador("Enzo");
 jogador1.Cartas.Add(carta1);
+
+Jogo jogo = new Jogo(jogador1, jogador2);
 
 Console.WriteLine(jogador1.nome);
 Console.WriteLine(jogador1.Cartas[0].Nome);
